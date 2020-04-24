04d971b59fb2ebfbfc821a55a2080390bca51389
realwhyjay <brandon94_@naver.com>
2020-04-25 00:56:58 +0900

    navigation을 사용하지 않은 희대의 실패작

---
+++ HW1-1 (실패)/HW1-1Tests/HW1_1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  HW1_1Tests.swift
+//  HW1-1Tests
+//
+//  Created by 최영재 on 18/04/2020.
+//  Copyright © 2020 최영재. All rights reserved.
+//
+
+import XCTest
+@testable import HW1_1
+
+class HW1_1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
