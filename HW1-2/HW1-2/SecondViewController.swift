ba327efa8321913f6d27e215bf1aed6469d130f0
realwhyjay <brandon94_@naver.com>
2020-04-25 00:58:05 +0900

    로그인 화면

---
+++ HW1-2/HW1-2/SecondViewController.swift
@@ -0,0 +1,42 @@
+//
+//  SecondViewController.swift
+//  HW1-2
+//
+//  Created by 최영재 on 18/04/2020.
+//  Copyright © 2020 최영재. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    @IBAction func loginDataTransfer(_ sender: Any) {
+        guard let receiveViewController = self.storyboard?.instantiateViewController(identifier: "ThirdViewController") as? ThirdViewController
+                   else {return}
+               
+               receiveViewController.id = idTextField.text
+               receiveViewController.password = passwordTextField.text
+               receiveViewController.modalPresentationStyle = .fullScreen
+            self.present(receiveViewController, animated: true, completion: nil)
+                   
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ HW1-2/HW1-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW1-2
+//
+//  Created by 최영재 on 18/04/2020.
+//  Copyright © 2020 최영재. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
